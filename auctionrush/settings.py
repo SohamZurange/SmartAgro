0394a4ffb811c3c82e7d40a234b04d266db7f95a
Soham_Zurange <54995067+Soham-Zurange@users.noreply.github.com>
2022-05-23 10:36:17 +0530

    commit

--- auctionrush/settings.py
+++ auctionrush/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = '_jjhm#&qjqbm_qula%xs2(0&^qwai@2c%au&4z57%g+=-lsbno'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- auctionrush/urls.py
+++ auctionrush/urls.py
@@ -19,6 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 from auctions import views as auctions_views
+from django.views.static import serve
+from django.conf.urls import url
 
 admin.site.site_header = "Farming System Admin"
 admin.site.site_title = "Agriculture Admin Portal"
@@ -39,4 +41,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path("accounts/register", views.register, name="register"),
     path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
